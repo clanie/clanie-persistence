e6e1405c1d74941a9fabb558250447077a1e16ca
Claus Nielsen <cni@steria.dk>
2012-07-09 12:26:09 +0200

    Removed Hibernate dependencies and Hibernate specific classes. Added JPA dependency.

--- src/main/java/dk/clanie/hibernate/usertype/package-info.java
+++
@@ -1,7 +0,0 @@
-@org.hibernate.annotations.TypeDefs({
-	@org.hibernate.annotations.TypeDef(
-			name = "url",
-			typeClass = dk.clanie.hibernate.usertype.UrlUserType.class
-	)
-})
-package dk.clanie.hibernate.usertype;
